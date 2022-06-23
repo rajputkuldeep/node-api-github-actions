b500c754dcdee8a702d696ed0af58c452892940f
rajputkuldeep <kuldeep.rajput@300minds.com>
2022-06-23 10:32:38 +0530

    commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (_, res) => {
-    res.send('<html> <head>server Response🙂</head><body><h1> This page was render direcly from the server <p>Hello there welcome to my website</p></h1></body></html>');
+    res.send('<html> <head>server Response 🙂</head><body><h1> This page was render direcly from the server <p>Hello there welcome to my website</p></h1></body></html>');
 });
 
 const server = app.listen(PORT, HOST, () => {
